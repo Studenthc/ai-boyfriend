a44b64174483db440ea4a0718224fa13d9733e0c
Even <studenthc@163.com>
2024-09-23 23:31:50 +0800

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,31 @@ const nextConfig = {
     async redirects() {
         return [
             {
+                source: '/search/fb.php',
+                destination: '/',
+                permanent: true,
+            },
+            {
+                source: '/_next/static/media/a34f9d1faa5f3315-s.p.woff2',
+                destination: '/',
+                permanent: true,
+            },
+            {
+                source: '/parking.php',
+                destination: '/',
+                permanent: true,
+            },
+            {
+                source: '/search/tsc.php',
+                destination: '/',
+                permanent: true,
+            },
+            {
+                source: '/search/cc.php',
+                destination: '/',
+                permanent: true,
+            },
+            {
                 source: '/:path*',
                 has: [
                     {
